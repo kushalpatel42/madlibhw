092f1354406478919ca89bafb2c693cfe8f8f118
kushalpatel42 <86609266+kushalpatel42@users.noreply.github.com>
2021-07-21 14:35:54 -0400

    leave some message

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-
 function createParagraph(){
+    var wordOne = document.getElementById('wordOne').value;
+    // var wordTwo = document.getElementById("wordTwo").value;
 
 
+var paragraph = "<p> Today, every student has a computer small enough to fit into his  " + wordOne +" .</p>" ;
 
 
-    
+document.getElementById("answer").innerHTML = paragraph;
 }
+
